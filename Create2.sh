cb7e2ad93e438974ef743005a3fea9a5501e05f5
Quthbulhameed DJIAVOUDINE <97978814+Quthbulhameed@users.noreply.github.com>
2023-07-17 14:37:02 +0200

    Update Create2.sh

--- Create2.sh
+++ Create2.sh
@@ -17,6 +17,15 @@ for i in {1..2}; do
   done
 done
 
+
+# Install packages and extensions inside the containers
+for i in {1..2}; do
+  sudo lxc-attach -n container"$i" -- apt-get update
+  sudo lxc-attach -n container"$i" -- sudo apt-get install apache2 mysql-server php 
+  ###sudo lxc-attach -n container"$i" -- apt-get install -y apache2 mysql-server php php-mysql php-cli php-json php-opcache php-gd php-curl php-mbstring php-xml php-xmlrpc php-soap php-intl php-zip
+done
+
+
 # Print the IP addresses of all the containers
 for i in {1..2}; do
   echo "container-$i IP: $(lxc-info -n container"$i" | grep -Eo 'IP\s+:\s(.*)' | awk '{print $2}')"
